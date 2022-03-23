a42f554b67dc5c071f826fd2f0f9a34e980f18f6
Augusto Cesar <acs-03@hotmail.com>
2022-03-23 18:50:52 -0300

    Conditional structure - logic expressions

---
+++ src/_03_conditional_structure/_02_logic_expressions.java
@@ -0,0 +1,25 @@
+package _03_conditional_structure;
+
+public class _02_logic_expressions {
+
+	public static void main(String[] args) {
+		
+		int number1 = 15;
+		int number2 = 10;
+		int number3 = 20;
+		
+		System.out.println("END:");
+		System.out.printf("(%d > %d) && (%d < %d) = %b %n", number1, number2, number1, number3, ((number1 > number2) && (number1 < number3)));
+		System.out.printf("(%d > %d) && (%d > %d) = %b %n%n", number1, number2, number1, number3, ((number1 > number2) && (number1 > number3)));
+		
+		System.out.println("OR:");
+		System.out.printf("(%d > %d) || (%d < %d) = %b %n", number1, number2, number1, number3, ((number1 > number2) || (number1 < number3)));
+		System.out.printf("(%d > %d) || (%d > %d) = %b %n%n", number1, number2, number1, number3, ((number1 > number2) || (number1 > number3)));
+		
+		System.out.println("NOT:");
+		System.out.printf("!(%d < %d) = %b %n", number1, number2, (!(number1 > number2)));
+		System.out.printf("!(%d = %d) = %b %n%n", number1, number2, (!(number1 == number2)));
+
+	}
+
+}
