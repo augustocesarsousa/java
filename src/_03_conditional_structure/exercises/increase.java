04320ebf98d886475f7fcb3902d1c35075a96201
Augusto Cesar <acs-03@hotmail.com>
2022-03-24 18:43:04 -0300

    Conditional structure - increase exercise

---
+++ src/_03_conditional_structure/exercises/increase.java
@@ -0,0 +1,42 @@
+package _03_conditional_structure.exercises;
+
+import java.util.Scanner;
+
+public class increase {
+
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+
+    double salary, increase;
+    int percentage;
+
+    System.out.println("Enter your salary:");
+    salary = scanner.nextDouble();
+
+    if (salary <= 1000.0) {
+      percentage = 20;
+      increase = salary / 100 * percentage;
+      salary += increase;
+    } else if (salary <= 3000.0) {
+      percentage = 15;
+      increase = salary / 100 * percentage;
+      salary += increase;
+    } else if (salary <= 8000.0) {
+      percentage = 10;
+      increase = salary / 100 * percentage;
+      salary += increase;
+    } else {
+      percentage = 5;
+      increase = salary / 100 * percentage;
+      salary += increase;
+    }
+
+    System.out.printf("Your new salary is $ %.2f\n", salary);
+    System.out.printf("Increase = %.2f\n", increase);
+    System.out.printf("Percentage = %d %%\n", percentage);
+
+    scanner.close();
+
+  }
+}
