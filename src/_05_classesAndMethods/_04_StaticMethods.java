780dc4907b9b98f2f8737d37843f10d53b9475fc
Augusto Cesar <acs-03@hotmail.com>
2022-03-28 18:34:44 -0300

    Classes and Methods - Static methods

---
+++ src/_05_classesAndMethods/_04_StaticMethods.java
@@ -0,0 +1,28 @@
+package _05_classesAndMethods;
+
+import java.util.Scanner;
+
+import _05_classesAndMethods.utils.Calculator;
+
+public class _04_StaticMethods {
+
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+    double radius, circumference, volume;
+
+    System.out.print("Enter radius: ");
+    radius = scanner.nextDouble();
+
+    circumference = Calculator.circumference(radius);
+    volume = Calculator.volume(radius);
+
+    System.out.println();
+    System.out.printf("Circumference: %.2f%n", circumference);
+    System.out.printf("Volume: %.2f%n", volume);
+    System.out.printf("PI value: %.2f%n", Calculator.PI);
+
+    scanner.close();
+
+  }
+}
