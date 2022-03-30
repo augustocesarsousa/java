ee88d78afaab19c5cecce7c9668e599e8036362d
Augusto Cesar <acs-03@hotmail.com>
2022-03-30 18:03:41 -0300

    Contructor, this, overhead and encapsulation - Overhead

---
+++ src/_06_constructorThisOverheadEncapsulation/entities/Car2.java
@@ -0,0 +1,31 @@
+package _06_constructorThisOverheadEncapsulation.entities;
+
+public class Car2 {
+
+  public String name;
+  public String color;
+  public double price;
+
+  public Car2() {
+  }
+
+  public Car2(String name, String color) {
+    this.name = name;
+    this.color = color;
+  }
+
+  public Car2(String name, String color, double price) {
+    this.name = name;
+    this.color = color;
+    this.price = price;
+  }
+
+  @Override
+  public String toString() {
+    return name
+        + ", "
+        + color
+        + ", $"
+        + String.format("%.2f\n", price);
+  }
+}
