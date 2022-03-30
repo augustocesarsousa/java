d3a1a5a08521933cd0b7bb143d3a2cc8615706aa
Augusto Cesar <acs-03@hotmail.com>
2022-03-29 22:58:22 -0300

    Repetitive structure - odd exercise

---
+++ src/_04_repetitiveStructure/exercice/Odd.java
@@ -0,0 +1,26 @@
+package _04_repetitiveStructure.exercice;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Odd {
+    
+    public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner scanner = new Scanner(System.in);
+		
+		int x;
+
+	    System.out.print("Enter the value of X: ");
+	    x = scanner.nextInt();
+
+	    for (int i=1; i<=x; i++) {
+	        if (i % 2 != 0) {
+	        	System.out.printf("%d\n", i);
+	        }
+	    }
+
+		scanner.close();
+	}
+}
