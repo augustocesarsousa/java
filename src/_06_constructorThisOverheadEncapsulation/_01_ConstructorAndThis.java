fe140e2eddacfafe3901f906940f93a9108d9a65
Augusto Cesar <acs-03@hotmail.com>
2022-03-30 17:57:41 -0300

    Contructor, this, overhead and encapsulation - Contructor and this

---
+++ src/_06_constructorThisOverheadEncapsulation/_01_ConstructorAndThis.java
@@ -0,0 +1,31 @@
+package _06_constructorThisOverheadEncapsulation;
+
+import java.util.Scanner;
+
+import _06_constructorThisOverheadEncapsulation.entities.Car;
+
+public class _01_ConstructorAndThis {
+
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+    String name;
+    String color;
+    double price;
+
+    System.out.print("Car name: ");
+    name = scanner.nextLine();
+
+    System.out.print("Car color: ");
+    color = scanner.nextLine();
+
+    System.out.print("Car price: ");
+    price = scanner.nextDouble();
+
+    Car car = new Car(name, color, price);
+    System.out.println(car);
+    scanner.close();
+
+  }
+
+}
---
+++ src/_06_constructorThisOverheadEncapsulation/entities/Car.java
@@ -0,0 +1,23 @@
+package _06_constructorThisOverheadEncapsulation.entities;
+
+public class Car {
+
+  public String name;
+  public String color;
+  public double price;
+
+  public Car(String name, String color, double price) {
+    this.name = name;
+    this.color = color;
+    this.price = price;
+  }
+
+  @Override
+  public String toString() {
+    return name
+        + ", "
+        + color
+        + ", $"
+        + String.format("%.2f\n", price);
+  }
+}
