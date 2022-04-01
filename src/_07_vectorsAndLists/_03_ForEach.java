0d679f8f63a646d42357635483da03f37dfd7a9a
Augusto Cesar <acs-03@hotmail.com>
2022-04-01 17:13:29 -0300

    Vectors and Lists - For each

---
+++ src/_07_vectorsAndLists/_03_ForEach.java
@@ -0,0 +1,13 @@
+package _07_vectorsAndLists;
+
+public class _03_ForEach {
+
+  public static void main(String[] args) {
+
+    String[] names = new String[] { "Sushi", "Zeus", "Nino" };
+
+    for (String name : names) {
+      System.out.println(name);
+    }
+  }
+}
