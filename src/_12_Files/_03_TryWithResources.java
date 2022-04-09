038b2e834b38e3192cfaa1e377f7841088b22013
Augusto Cesar <acs-03@hotmail.com>
2022-04-09 16:55:23 -0300

    Files - Try with resources

---
+++ src/_12_Files/_03_TryWithResources.java
@@ -0,0 +1,25 @@
+package _12_Files;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * _03_TryWithResources
+ */
+public class _03_TryWithResources {
+  public static void main(String[] args) {
+    String path = "/home/augusto/Documents/test.txt";
+
+    try (BufferedReader bufferedReader = new BufferedReader(new FileReader(path))) {
+      String line = bufferedReader.readLine();
+
+      while (line != null) {
+        System.out.println(line);
+        line = bufferedReader.readLine();
+      }
+    } catch (IOException e) {
+      System.out.println("Error: " + e.getMessage());
+    }
+  }
+}
