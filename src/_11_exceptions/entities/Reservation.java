c5792ef1a6b365349def28cb3052d3c82a9b52f5
Augusto Cesar <acs-03@hotmail.com>
2022-04-09 00:50:12 -0300

    Exceptions - Personalized exception

---
+++ src/_11_exceptions/entities/Reservation.java
@@ -0,0 +1,82 @@
+package _11_exceptions.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import _11_exceptions.exceptions.DomainException;
+
+/**
+ * Reservation
+ */
+public class Reservation {
+
+  private Integer roomNumber;
+  private Date checkIn;
+  private Date checkOut;
+
+  private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat(
+    "dd/MM/yyyy"
+  );
+
+  public Reservation() {}
+
+  public Reservation(Integer roomNumber, Date checkIn, Date checkOut)
+    throws DomainException {
+    Date now = new Date();
+    if (checkIn.before(now) || checkOut.before(now)) {
+      throw new DomainException("Reservation dates must be future dates");
+    }
+    this.roomNumber = roomNumber;
+    this.checkIn = checkIn;
+    this.checkOut = checkOut;
+  }
+
+  public Integer getRoomNumber() {
+    return roomNumber;
+  }
+
+  public void setRoomNumber(Integer roomNumber) {
+    this.roomNumber = roomNumber;
+  }
+
+  public Date getCheckIn() {
+    return checkIn;
+  }
+
+  public Date getCheckout() {
+    return checkOut;
+  }
+
+  public long duration() {
+    long diff = checkOut.getTime() - checkIn.getTime();
+    return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+  }
+
+  public void updateDates(Date checkIn, Date checkOut) throws DomainException {
+    Date now = new Date();
+    if (checkIn.before(now) || checkOut.before(now)) {
+      throw new DomainException("Reservation dates must be future dates");
+    }
+    if (!checkOut.after(checkIn)) {
+      throw new DomainException("Check-out date must be after check-in date");
+    }
+    this.checkIn = checkIn;
+    this.checkOut = checkOut;
+  }
+
+  @Override
+  public String toString() {
+    return (
+      "Reservation: Room " +
+      roomNumber +
+      ", check-in: " +
+      simpleDateFormat.format(checkIn) +
+      ", check-out: " +
+      simpleDateFormat.format(checkOut) +
+      ", " +
+      duration() +
+      " nights"
+    );
+  }
+}
---
+++ src/_11_exceptions/exceptions/DomainException.java
@@ -0,0 +1,11 @@
+package _11_exceptions.exceptions;
+
+/**
+ * DomainException
+ */
+public class DomainException extends Exception {
+
+  public DomainException(String msg) {
+    super(msg);
+  }
+}
