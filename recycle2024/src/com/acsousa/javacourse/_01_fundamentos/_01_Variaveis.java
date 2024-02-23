fd144c7c160e303abdf902d8b48af0fadc2dbc50
Augusto Cesar <acs-03@hotmail.com>
2024-02-22 21:57:21 -0300

    Reciclagem Java 2024 - Fundamentos: Variáveis

---
+++ recycle2024/src/com/acsousa/javacourse/_01_fundamentos/_01_Variaveis.java
@@ -0,0 +1,29 @@
+package com.acsousa.javacourse._01_fundamentos;
+
+public class _01_Variaveis {
+
+  public static void main(String[] args) {
+    /*
+      Uma variável é o elemento responsável por armazenar um valor na memória do computador
+      No Java a convenção recomenda a criação de uma variável por linha
+      Os nomes das variáveis devem seguir o padrão lowerSnakeCase, exemplo, umaVariavel
+      Os nomes das variáveis não podem iniciar com dígitos, exemplo, 1variavel
+      Toda variável em Java deve possuir um tipo
+     */
+
+    //Declarando três variáveis do tipo numérico inteiro
+    int numero1;
+    int numero2;
+    int resultado;
+
+    //Atribuindo valores para as variáveis
+    numero1 = 15;
+    numero2 = 11;
+
+    //Executando uma operação aritmética de soma e atribuindo o valor para a variavel resultado
+    resultado = numero1 + numero2;
+
+    //Imprimindo o valor da variável resultado
+    System.out.println(resultado);
+  }
+}
