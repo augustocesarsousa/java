b2058eb23d970cdf389e91644056e0627d69dfb5
Augusto Cesar <acs-03@hotmail.com>
2024-02-23 19:58:20 -0300

    Reciclagem Java 2024 - Fundamentos: Operadores Aritméticos

---
+++ recycle2024/src/com/acsousa/javacourse/_01_fundamentos/_02_OperadoresAritmeticos.java
@@ -0,0 +1,30 @@
+package com.acsousa.javacourse._01_fundamentos;
+
+public class _02_OperadoresAritmeticos {
+  public static void main(String[] args) {
+
+    /*
+      No Java os operadores aritmédicos básicos são cinco
+      Adição (+)
+      Subtração (-)
+      Multiplicação (*)
+      Divisão (/)
+      Módulo ou resto (%), que é a sobra da divisão entre dois números
+     */
+
+    //Somando dois números
+    System.out.println("15 + 2 = " + (15 + 13));
+
+    //Subtraindo dois números
+    System.out.println("15 - 2 = " + (15 - 13));
+
+    //Multiplicando dois números
+    System.out.println("15 * 2 = " + (15 * 2));
+
+    //Dividindo dois números
+    System.out.println("15 / 2 = " + (15 / 2));
+
+    //Resto da divisão entre dois números
+    System.out.println("15 % 2 = " + (15 % 2));
+  }
+}
