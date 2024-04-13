495b95f310e48a78a1949a0c7f39e1950f66eac7
Augusto Cesar <acs-03@hotmail.com>
2024-04-13 19:41:07 -0300

    Reciclagem Java 2024 - Estrutura de controle e operadores: Operadores de comparação

---
+++ recycle2024/src/com/acsousa/javacourse/_02_estrutura_de_controle_e_operadores/_02_OperadoresDeComparacao.java
@@ -0,0 +1,45 @@
+package com.acsousa.javacourse._02_estrutura_de_controle_e_operadores;
+
+public class _02_OperadoresDeComparacao {
+  public static void main(String[] args) {
+
+    /*
+      Podemos fazer comparação de dados em Java com os seguintes comparadores:
+      Maior que >
+      Meior ou igual >=
+      Menor que <
+      Menor ou igual <=
+     */
+
+    // Declarando variáveis de teste
+    int numero1 = 10;
+    int numero2 = 10;
+    int numero3 = 11;
+    boolean resultado;
+
+    // Comparações
+    resultado = numero1 > numero2;
+    System.out.printf("%d é maior que %d? %b%n", numero1, numero2, resultado);
+
+    resultado = numero1 >= numero2;
+    System.out.printf("%d é maior ou igual à %d? %b%n", numero1, numero2, resultado);
+
+    resultado = numero1 < numero2;
+    System.out.printf("%d é menor que %d? %b%n", numero1, numero2, resultado);
+
+    resultado = numero1 <= numero2;
+    System.out.printf("%d é menor ou igual à %d? %b%n", numero1, numero2, resultado);
+
+    resultado = numero1 > numero3;
+    System.out.printf("%d é maior que %d? %b%n", numero1, numero3, resultado);
+
+    resultado = numero1 >= numero3;
+    System.out.printf("%d é maior ou igual à %d? %b%n", numero1, numero3, resultado);
+
+    resultado = numero1 < numero3;
+    System.out.printf("%d é menor que %d? %b%n", numero1, numero3, resultado);
+
+    resultado = numero1 <= numero3;
+    System.out.printf("%d é menor ou igual à %d? %b%n", numero1, numero3, resultado);
+  }
+}
