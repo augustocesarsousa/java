9fa396e53d704ba139b6f4204a1c542f24bc2305
Augusto Cesar <acs-03@hotmail.com>
2024-04-16 21:15:36 -0300

    Reciclagem Java 2024 - Estrutura de controle e operadores: Operadores condicionais IF e ELSE

---
+++ recycle2024/src/com/acsousa/javacourse/_02_estrutura_de_controle_e_operadores/_04_OperadorCondicionalIfElse.java
@@ -0,0 +1,41 @@
+package com.acsousa.javacourse._02_estrutura_de_controle_e_operadores;
+
+public class _04_OperadorCondicionalIfElse {
+
+  public static void main(String[] args) {
+
+    /*
+      Em Java os operadores condicionais SE e SENÃO são representados pelos operadores
+      IF e ELSE
+     */
+
+    // Declaração de variáveis para testes
+    int media = (int) (Math.random() * 10);
+    int numero = (int) (Math.random() * 10);
+
+    // Testes IF
+    if (numero < media) {
+      System.out.printf("O número %d é menor que a média!%n", numero);
+    }
+
+    if (numero == media) {
+      System.out.printf("O número %d é igual a média!%n", numero);
+    }
+
+    if (numero > media) {
+      System.out.printf("O número %d é meior que a média!%n", numero);
+    }
+
+    // Testes IF e ELSE
+    media = (int) (Math.random() * 10);
+    numero = (int) (Math.random() * 10);
+
+    if (numero < media) {
+      System.out.printf("O número %d é menor que a média!%n", numero);
+    } else if (numero == media) {
+      System.out.printf("O número %d é igual a média!%n", numero);
+    } else {
+      System.out.printf("O número %d é meior que a média!%n", numero);
+    }
+  }
+}
