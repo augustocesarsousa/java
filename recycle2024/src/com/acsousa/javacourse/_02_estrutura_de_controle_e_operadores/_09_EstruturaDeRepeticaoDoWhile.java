3e16509639bc04589fb12ff36cb1d7f1e9cf89d1
Augusto Cesar <acs-03@hotmail.com>
2024-04-25 21:02:58 -0300

    Reciclagem Java 2024 - Estrutura de controle e operadores: Estrutura de repetição Do While

---
+++ recycle2024/src/com/acsousa/javacourse/_02_estrutura_de_controle_e_operadores/_09_EstruturaDeRepeticaoDoWhile.java
@@ -0,0 +1,21 @@
+package com.acsousa.javacourse._02_estrutura_de_controle_e_operadores;
+
+public class _09_EstruturaDeRepeticaoDoWhile {
+
+  public static void main(String[] args) {
+
+    /*
+      A estrutura de repetição Do While permite que possamos criar loops de repetição
+      onde a condição precise ser executada pelo menos uma vez, mesmo sendo falsa
+     */
+
+    // Declaração de variável para teste
+    int numero = (int) (Math.random() * 10);
+
+    // Teste
+    do {
+      System.out.println("Número " + numero + " é par? " + (numero % 2 == 0));
+      numero = (int) (Math.random() * 10);
+    } while (numero % 2 == 0);
+  }
+}
