91e70fbcdebac00bd0fa44c4eb0ff49297eba4e5
Augusto Cesar <acs-03@hotmail.com>
2024-04-26 07:58:48 -0300

    Reciclagem Java 2024 - Estrutura de controle e operadores: Cláusulas Break e Continue

---
+++ recycle2024/src/com/acsousa/javacourse/_02_estrutura_de_controle_e_operadores/_10_ClausulaBreakEContinue.java
@@ -0,0 +1,36 @@
+package com.acsousa.javacourse._02_estrutura_de_controle_e_operadores;
+
+public class _10_ClausulaBreakEContinue {
+
+  public static void main(String[] args) {
+
+    /*
+      As clausulas Break e Continue servem para controlar o fluxo de um loop
+      Break: usado para parar o fluxo e sair do loop
+      Continue: usado para pular para a próxima iteração do loop
+     */
+
+    // Declaração de variáveis para teste
+    int numero = 0;
+
+    // Testes
+    System.out.printf("Break:%n%n");
+    for (int i = 0;i < 10; i++) {
+      if(i == 7) {
+        break;
+      }
+
+      System.out.println("3 x " + i + " = " + (3 * i));
+    }
+
+    System.out.printf("%nContinue:%n%n");
+
+    for (int i = 0;i < 10; i++) {
+      if(i == 5) {
+        continue;
+      }
+
+      System.out.println("3 x " + i + " = " + (3 * i));
+    }
+  }
+}
