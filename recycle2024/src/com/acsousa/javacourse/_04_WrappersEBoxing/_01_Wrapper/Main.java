ed5cf1043f6ae6e6fa3c54dfd48f9b4d6135a36d
Augusto Cesar <acs-03@hotmail.com>
2024-05-13 23:17:34 -0300

    Reciclagem Java 2024 - Wrapper e Boxing: Wrapper

---
+++ recycle2024/src/com/acsousa/javacourse/_04_WrappersEBoxing/_01_Wrapper/Main.java
@@ -0,0 +1,16 @@
+package com.acsousa.javacourse._04_WrappersEBoxing._01_Wrapper;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    Wrappers wrappers = new Wrappers();
+
+    System.out.println("Inteiro: " + wrappers.numeroWrapperInteiro);
+    System.out.println("Long: " + wrappers.numeroWrapperLongo);
+    System.out.println("Double: " + wrappers.numeroWrapperDouble);
+    System.out.println("Float: " + wrappers.numeroWrapperFloat);
+    System.out.println("Char: " + wrappers.caracterWrapper);
+    System.out.println("Booleano: " + wrappers.booleanWrapper);
+  }
+}
---
+++ recycle2024/src/com/acsousa/javacourse/_04_WrappersEBoxing/_01_Wrapper/Wrappers.java
@@ -0,0 +1,29 @@
+package com.acsousa.javacourse._04_WrappersEBoxing._01_Wrapper;
+
+public class Wrappers {
+
+  /*
+      Classes wrapper em Java são como caixas especiais que "embalam" tipos de dados primitivos.
+      Elas fornecem funcionalidades extras que os dados primitivos por si só não possuem.
+      Uma característica de uma classe wrapper é que o valor inicial delas é null.
+     */
+
+  // Wrapper para números inteiros
+  Integer numeroWrapperInteiro;
+
+  // Wrapper para números longos
+  Long numeroWrapperLongo;
+
+  // Wrapper para números com ponto flutuantes
+  Double numeroWrapperDouble;
+
+  // Wrapper para números com ponto flutuantes longos
+  Float numeroWrapperFloat;
+
+  // Wrapper para caractéres
+  Character caracterWrapper;
+
+  // Wrapper para boleanos
+  Boolean booleanWrapper;
+
+}
