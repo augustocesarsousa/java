e4d3d12206e4a08bdfa010531dd67da9b1d3f568
Augusto Cesar <acs-03@hotmail.com>
2024-05-17 17:32:19 -0300

    Reciclagem Java 2024 - Arrays: Declaração

---
+++ recycle2024/src/com/acsousa/javacourse/_05_Arrays/_01_DeclarandoUmArray/Main.java
@@ -0,0 +1,25 @@
+package com.acsousa.javacourse._05_Arrays._01_DeclarandoUmArray;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    /*
+      Em Java, um vetor, também conhecido como array, é uma estrutura de dados que armazena uma coleção ordenada
+      de elementos do mesmo tipo. Imagine uma caixa com compartimentos numerados, onde cada compartimento guarda
+      um elemento específico.
+     */
+
+    // Formas de declarar um array
+    
+    // Declaração sem atribuição
+    int[] array1 = new int[10];
+
+    // Declaração com atribuição na mesma linha
+    int[] array2 = {1, 2, 3, 4, 5};
+
+    // Declaração com atribuição em linhas diferentes
+    int[] array3;
+    array3 = new int[]{6, 7, 8, 9, 10};
+  }
+}
